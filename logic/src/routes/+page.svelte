1327517f6d6c65b47cd45f8e20dc53ef93ee72c2
hyeon <hyeon@planetariumhq.com>
2023-01-17 01:55:21 +0900

    Use logic using "{# }" block

---
+++ logic/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  let user = {loggedIn: false};
+
+  const toggle = () => {
+    user.loggedIn = !user.loggedIn;
+  }
+</script>
+
+<div>User login status : {user.loggedIn}</div>
+
+{#if !user.loggedIn}
+    <button on:click={toggle}>Log In</button>
+{/if}
+{#if user.loggedIn}
+    <button on:click={toggle}>Log Out</button>
+{/if}
