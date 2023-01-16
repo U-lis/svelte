31c9bcd824275e22571519dfadd430037fe7a824
hyeon <hyeon@planetariumhq.com>
2023-01-17 01:25:54 +0900

    First reactivity: handle click count

---
+++ reactivity/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  const handleIncrement = () => {
+    count += 1;
+  };
+
+  const resetCount = () => {
+    count = 0;
+  };
+</script>
+
+<div>
+    Click count: {count} {count <= 1 ? "time" : "times"}
+</div>
+<button on:click={handleIncrement}>Click!</button>
+<button on:click={resetCount}>Reset</button>
