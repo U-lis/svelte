61ed93030b796b782d2832ec5a122006b91efa71
hyeon <hyeon@planetariumhq.com>
2023-01-17 02:00:28 +0900

    else if

--- logic/src/routes/+page.svelte
+++ logic/src/routes/+page.svelte
@@ -4,6 +4,14 @@
   const toggle = () => {
     user.loggedIn = !user.loggedIn;
   }
+
+  let number = 0;
+  const increment = () => {
+    number += 1;
+  };
+  const reset = () => {
+    number = 0;
+  }
 </script>
 
 <div>User login status : {user.loggedIn}</div>
@@ -13,3 +21,18 @@
 {:else}
     <button on:click={toggle}>Log Out</button>
 {/if}
+
+<hr>
+
+<div>Number: {number}</div>
+<div>
+    {#if number >= 10}
+        Too big!
+    {:else if number >= 5}
+        Big enough
+    {:else}
+        Good
+    {/if}
+</div>
+<button on:click={increment}>Click</button>
+<button on:click={reset}>Reset</button>
