b4402254ada7cf3be19d1b65fa982041dad7688d
hyeon <hyeon@planetariumhq.com>
2023-01-17 01:50:40 +0900

    Spreading is available

--- props/src/routes/+page.svelte
+++ props/src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
-  import Nested from "../components/Nested.svelte"
+  import Nested from "../components/Nested.svelte";
+  import Info from "../components/Info.svelte";
+
+  const pkg = {
+    name: 'svelte',
+    version: 3,
+    speed: 'blazing',
+    website: 'https://svelte.dev'
+  };
 </script>
 
 <Nested/>
+<Info {...pkg}></Info>
