6d961bf1e7aea58e0b8fbb85c6a98822f75c6dd1
hyeon <hyeon@planetariumhq.com>
2023-01-17 01:41:00 +0900

    Reactivity only triggered by direct assignment

--- reactivity/src/routes/+page.svelte
+++ reactivity/src/routes/+page.svelte
@@ -17,9 +17,36 @@
       count = 9;
     }
   }
+
+  let numbers = [];
+  $: sum = numbers.reduce((t, n) => t + n, 0);
+  const addNumber = () => {
+    numbers = [...numbers, Math.floor(Math.random() * 10)];
+  };
+  const resetNumbers = () => {
+    numbers = [];
+  };
+
+  // Indirect assignment does not work
+  let obj;
+  const foo = obj.foo;
+  foo.bar = "baz";
+
+  // Indirect assignment does not work
+  const noop = (thing) => {
+    thing.foo.bar = "baz";
+    // But this makes work
+    thing = thing;
+  }
+  noop(obj);
+
 </script>
 
 <div> Click count: {count} {count <= 1 ? "time" : "times"} </div>
 <div> Doubled click count: {doubled} {doubled < 2 ? "time" : "times"} </div>
 <button on:click={handleIncrement}>Click!</button>
 <button on:click={resetCount}>Reset</button>
+<hr>
+<div>Sum of [ {numbers.join(" + ")} ] = {sum}</div>
+<button on:click={addNumber}>Add a number</button>
+<button on:click={resetNumbers}>Reset</button>
