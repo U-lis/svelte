48e7a5d256ca30249598e9ca26664c4d493c87a1
U-lis <ulismoon@gmail.com>
2023-01-18 00:20:09 +0900

    Modifier can alter event handler behaviour

--- events/src/routes/+page.svelte
+++ events/src/routes/+page.svelte
@@ -6,14 +6,19 @@
     m.x = e.clientX;
     m.y = e.clientY;
   }
+
+  const handleAlert = () => {
+    alert("You can see me just one time");
+  }
 </script>
 
 <div on:mousemove={handleMouseMove}>The mouse position is: {m.x}, {m.y}</div>
 <div on:mousemove={e=> mm = {x: e.clientX, y: e.clientY}}>The mouse position is: {mm.x}, {mm.y}</div>
+<button on:click|once={handleAlert}>One time alert</button>
 
 <style>
   div {
       width: 100%;
-      height: 50%;
+      height: 20%;
   }
 </style>
