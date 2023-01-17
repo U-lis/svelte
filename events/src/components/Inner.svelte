d78a6a7e558d84356e8e33c5dbe66001bd318554
U-lis <ulismoon@gmail.com>
2023-01-18 00:28:08 +0900

    Dispatch event one component to another component

---
+++ events/src/components/Inner.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {createEventDispatcher} from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  // button click calls this function
+  const sayHello = () => {
+    // dispatch message event > trigger on:message event
+    // See +page.svelte to find on:message
+    dispatch("message", {text: "hello"});
+  };
+</script>
+
+<button on:click={sayHello}>Click to dispatch event</button>
--- events/src/routes/+page.svelte
+++ events/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Inner from "../components/Inner.svelte";
+
   let m = {x: 0, y: 0};
   let mm = {x: 0, y: 0};
 
@@ -10,11 +12,19 @@
   const handleAlert = () => {
     alert("You can see me just one time");
   }
+
+  const handleMessage = (e) => {
+    // data from Inner.svelte: {text: "hello"} in e.detail
+    alert(e.detail.text);
+  };
 </script>
 
 <div on:mousemove={handleMouseMove}>The mouse position is: {m.x}, {m.y}</div>
 <div on:mousemove={e=> mm = {x: e.clientX, y: e.clientY}}>The mouse position is: {mm.x}, {mm.y}</div>
 <button on:click|once={handleAlert}>One time alert</button>
+
+<!-- dispatch message event from Inner.svelte -->
+<Inner on:message={handleMessage}></Inner>
 
 <style>
   div {
