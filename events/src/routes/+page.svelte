c8a9d6b47a82d8808477c746600d9297275f396e
U-lis <ulismoon@gmail.com>
2023-01-18 00:16:27 +0900

    Bind event to element

---
+++ events/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  let m = {x: 0, y: 0};
+
+  const handleMouseMove = (e) => {
+    m.x = e.clientX;
+    m.y = e.clientY;
+  }
+</script>
+
+<div on:mousemove={handleMouseMove}>The mouse position is: {m.x}, {m.y}</div>
+
+<style>
+  div {
+      width: 100%;
+      height: 50%;
+  }
+</style>
